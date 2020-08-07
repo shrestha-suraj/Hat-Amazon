14c18e86d47303ee1b226d146b20133a07ec128b
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-07 17:37:11 -0500

    Create homepage and its scss contents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+    <HomePage/>
     </div>
   );
 }
