5a767be8b4db8a62d14327a1337969a8c7995f7b
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-07 22:11:48 -0500

    Solved key issue on map set datas

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -14,7 +14,7 @@ class ShopPage extends React.Component {
     return (
       <div className="shop-page">
         {this.state.collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview id={id} {...otherCollectionProps}/>
+          <CollectionPreview key={id} {...otherCollectionProps}/>
         ))}
       </div>
     );
