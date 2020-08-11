c584585a72f4e86abc374dc6fa5fe420f07d4463
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-11 18:18:35 -0500

    Added count increment on cart-icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,14 +4,18 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.actions.js'
 
-const CartIcon=({toggleCartHidden})=>{
+const CartIcon=({toggleCartHidden,itemCount})=>{
     return(
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
+
+const matchStateToProps=({cart:{cartItems}})=>({
+    itemCount:cartItems.reduce((accumulator,item)=>accumulator+item.quantity,0)
+})
 
 const matchDispatchToProps=(dispatch)=>(
     {
@@ -19,4 +23,4 @@ const matchDispatchToProps=(dispatch)=>(
     }
 )
 
-export default connect(null,matchDispatchToProps)(CartIcon)
+export default connect(matchStateToProps,matchDispatchToProps)(CartIcon)
